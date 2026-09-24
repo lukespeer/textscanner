540b8a7f9d482b8fb0f50a47f276bc5eea76e7c3
Luke Speer <123386985+lukespeer@users.noreply.github.com>
2026-09-24 15:38:33 -0500

    add bounded text display

--- lib/screens/results_screen.dart
+++ lib/screens/results_screen.dart
@@ -212,6 +212,37 @@ class _SelectableTextImage extends StatelessWidget {
   double _area(Rect rect) => rect.width * rect.height;
 }
 
+class BoundsPainter extends CustomPainter {
+  final List<TextBlock> blocks;
+  final Size imageSize;
+
+  BoundsPainter({required this.blocks, required this.imageSize});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final scaleX = size.width / imageSize.width;
+    final scaleY = size.height / imageSize.height;
+
+    final paint = Paint()
+      ..color = Colors.red.withOpacity(0.5)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0;
+
+    for (var block in blocks) {
+      final rect = Rect.fromLTRB(
+        block.boundingBox.left * scaleX,
+        block.boundingBox.top * scaleY,
+        block.boundingBox.right * scaleX,
+        block.boundingBox.bottom * scaleY,
+      );
+      canvas.drawRect(rect, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
+
 class _EmptyResultsCard extends StatelessWidget {
   final ThemeData theme;
 
