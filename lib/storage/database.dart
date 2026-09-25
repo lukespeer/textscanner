b753ca02dc270cfbbe9cc36ced5811b94c90462f
nataliacastaneda <nataliamata706@gmail.com>
2026-09-24 20:14:25 -0500

    adds history database

--- lib/storage/database.dart
+++ lib/storage/database.dart
@@ -6,6 +6,21 @@ import 'package:sqflite/sqflite.dart';
 
 late Database database;
 
-void databaseInit() async {
-  database = await openDatabase(join(await getDatabasesPath(), 'history.db'));
+//call this once at startup, before saving or loading any results
+Future<void> databaseInit() async {
+  database = await openDatabase(
+    join(await getDatabasesPath(), 'history.db'),
+    version: 1,
+    onCreate: (db, version) async {
+      await db.execute('''
+        CREATE TABLE results(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          text TEXT,
+          imagePath TEXT,
+          dateTime TEXT
+        )
+      ''');
+    },
+  );
 }
+
